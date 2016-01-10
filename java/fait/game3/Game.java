a8e9e91ab7ebf8323b603ff8257e198b997a1c8e
Tedo Pranowo <tedokdr@yahoo.com>
2016-01-10 09:00:14 +0700

    Update and rename Game.java to java/fait/game3/Game.java
    Moving files to correct directory

--- java/fait/game3/Game.java
+++ java/fait/game3/Game.java
@@ -1,21 +1,21 @@
 package fait.game3;
 
 import android.app.Activity;
-import android.net.Uri;
+//import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 //import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+//import android.support.v7.widget.Toolbar;
+//import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.appindexing.Action;
+//import com.google.android.gms.appindexing.AppIndex;
+//import com.google.android.gms.common.api.GoogleApiClient;
 
 //public class Game extends AppCompatActivity {
 public class Game extends Activity {
