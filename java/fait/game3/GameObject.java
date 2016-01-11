36bbea2a8460cec6a7484cd3455dab2fd6d29d87
Tedo Pranowo <tedokdr@yahoo.com>
2016-01-12 05:53:54 +0700

    Create GameObject.java

---
+++ java/fait/game3/GameObject.java
@@ -0,0 +1,39 @@
+package fait.game3;
+
+import android.graphics.Rect;
+
+/**
+ * Created by Tedo on 1/10/2016.
+ */
+public class GameObject {
+    protected int x;
+    protected int y;
+    protected int dx;
+    protected int dy;
+    protected int width;
+    protected int height;
+
+    //getter and setters
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public int getWidth(){
+        return width;
+    }
+
+    public Rect getRectangle(){
+        return new Rect(x, y, x+width, y+height);
+    }
+}
